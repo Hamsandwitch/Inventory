76e25aff4e150f57fe87bdc83b162e5b9130bcea
Hamsandwitch <pddobso@gmail.com>
2016-08-11 01:29:12 -0400

    Added styling references/libraries
    Styling JS/CSS has been added, will need to be brought into the solution
    explorer.
    
    layout/styling content is still stock and will need to be wired.

--- Inventory/App_Start/BundleConfig.cs
+++ Inventory/App_Start/BundleConfig.cs
@@ -19,13 +19,46 @@ namespace Inventory
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
 						"~/Scripts/modernizr-*"));
 
-			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-					  "~/Scripts/bootstrap.js",
-					  "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                     "~/Scripts/bootstrap-switch.js",
+                     "~/Scripts/bootstrap.min.js",
+                     "~/Scripts/calendar-conf-events.js",
+                     "~/Scripts/chartjs-conf.js",
+                     "~/Scripts/common-scripts.js",
+                     "~/Scripts/easy-pie-chart.js",
+                     "~/Scripts/form-component.js",
+                     "~/Scripts/gritter-conf.js",
+                     "~/Scripts/jquery-1.8.3.min.js",
+                     "~/Scripts/jquery-easy-pie-chart",
+                     "~/Scripts/jquery-ui-1.9.2.custom.min.js",
+                     "~/Scripts/jquery.backstretch.min.js",
+                     "~/Scripts/jquery.dcjqaccordion.2.7.js",
+                     "~/Scripts/jquery.js",
+                     "~/Scripts/jquery.nicescroll.js",
+                     "~/Scripts/jquery.scrollTo.min.js",
+                     "~/Scripts/jquery.sparkline.js",
+                     "~/Scripts/jquery.tagsinput.js",
+                     "~/Scripts/jquery.ui.touch-punch.min.js",
+                     "~/Scripts/morris-conf.js",
+                     "~/Scripts/sparkline-chart.js",
+                     "~/Scripts/tasks.js",
+                     "~/Scripts/zabuto_calendar.js",
+                     "~/Scripts/bootstrap-inputmask/bootstrap-inputmask.min.js",
+                     "~/Scripts/chart-master/Chart.js",
+                     "~/Scripts/fancybox/jquery.fancybox.js",
+                     "~/Scripts/fullcalendar/fullcalendar.min.js",
+                     "~/Scripts/respond.js"));
 
-			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/bootstrap.css",
-					  "~/Content/site.css"));
-		}
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/style-responsive.css",
+                      "~/Content/style.css",
+                      "~/Content/table-responsive.css",
+                      "~/Content/to-do.css",
+                      "~/Content/font-awesome/css/font-awesome.css",
+                      "~/Content/gritter/css/jquery.gritter.css",
+                      "~/Content/lineicons/style.css",
+                      "~/Content/zabuto_calendar.css"));
+        }
 	}
 }
